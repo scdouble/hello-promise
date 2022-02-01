ad78218e4b68db1b3ae79a3b99e324fb0d88a666
Tatsu sei <doubledragontaz@gmail.com>
2022-02-01 21:26:57 +0900

    Promiseで関数をラップ

---
+++ 1.Promise-basic/5.util.promisify.js
@@ -0,0 +1,9 @@
+/**
+ * util.promisify
+ */
+const util = require('util')
+const fs = require("fs");
+let mineReadFile = util.promisify(fs.readFile)
+
+
+mineReadFile('./resource/content.txt').then(value => console.log(value.toString()))
