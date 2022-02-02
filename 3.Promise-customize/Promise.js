8dcf1318a5baa7fb74e61d884aa84815b8c96802
Tatsu sei <doubledragontaz@gmail.com>
2022-02-02 15:49:12 +0900

    非同期型処理Promiseの実行

--- 3.Promise-customize/Promise.js
+++ 3.Promise-customize/Promise.js
@@ -2,25 +2,41 @@ function Promise(executor) {
   // 属性を追加
   this.PromiseState = "pending";
   this.PromiseResult = null;
+  this.callback = {};
   // thisの値を保存
   const self = this;
 
   function resolve(data) {
     // 状態がすでに更新していたかどうかを判断する
-    if (self.PromiseState !== "pending") return;
+    if (self.PromiseState !== "pending") {
+      return;
+    }
     // 状態を変更(PromiseState)
     self.PromiseState = "fulfilled";
     // 結果を設定(PromiseResult)
     self.PromiseResult = data;
+    // 成功したコールバックを実行
+    if (self.callback.onResolved) {
+      self.callback.onResolved(data);
+    }
+
   }
+
   function reject(data) {
     // 状態がすでに更新していたかどうかを判断する
-    if (self.PromiseState !== "pending") return;
+    if (self.PromiseState !== "pending") {
+      return;
+    }
     // 状態を変更(PromiseState)
     self.PromiseState = "rejected";
     // 結果を設定(PromiseResult)
     self.PromiseResult = data;
+    // 失敗のコールバックを実行
+    if (self.callback.onResolved) {
+      self.callback.onRejected(data);
+    }
   }
+
   try {
     // 同期で実行
     executor(resolve, reject);
@@ -39,4 +55,12 @@ Promise.prototype.then = function (onResolved, onRejected) {
   if (this.PromiseState === "rejected") {
     onRejected(this.PromiseResult);
   }
+  if (this.PromiseState === "pending") {
+    //　コールバック関数を保存
+    this.callback = {
+      onResolved, onRejected
+    }
+
+  }
+
 };
